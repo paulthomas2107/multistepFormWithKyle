b5ccbe7ab93d1a316f265f64195ec6827832c404
Paul Thomas <paulthomas2107@gmail.com>
2022-11-21 11:26:48 +0000

    Steps

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const multiStepForm = document.querySelector('[data-multi-step]');
 const formSteps = [...multiStepForm.querySelectorAll('[data-step]')];
+
 let currentStep = formSteps.findIndex((step) => {
   return step.classList.contains('active');
 });
 
 if (currentStep < 0) {
   currentStep = 0;
-  formSteps[currentStep].classList.add('active');
+  showCurrentStep();
+}
+
+multiStepForm.addEventListener('click', (e) => {
+  if (e.target.matches('[data-next]')) {
+    currentStep += 1;
+  } else if (e.target.matches('[data-previous]')) {
+    currentStep -= 1;
+  }
+  showCurrentStep();
+});
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle('active', index === currentStep);
+  });
 }
